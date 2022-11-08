39c102e3127276e81d5f2ca9d7270023c1f5da50
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-08 05:40:11 -0800

    Add files via upload

---
+++ days29_tuple.py
@@ -0,0 +1,19 @@
+#tuple
+
+data_list = [10,3,4,5] # ini merupakan data lits menggunakan kurung siku
+print (data_list)
+
+data_tuple = (12,3,4) # ini merupakan data tuple menggunkan kurung biasa
+print(data_tuple)
+
+#ciri cirinya
+#menggunakan kurung biasa, tidak bisa melakukan append, tidak dapat melakukan pergantian elemen list
+
+#set
+data_set = {12,3,4,5,6} # data himpunan tidak ada urutan indexnya
+print ( data_set)
+
+
+
+
+
